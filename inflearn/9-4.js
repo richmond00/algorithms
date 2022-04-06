9940c5bbd019fa60c873400446fea55d3fd80aae
Dongwook <dongwook227@gmail.com>
2022-04-06 12:59:32 -0400

    add 9-4: bfs

---
+++ inflearn/9-4.js
@@ -0,0 +1,25 @@
+const solution = () => {
+  let answer = "";
+  let queue = [];
+  queue.push(1);
+
+  while (queue.length) {
+    let v = queue.shift();
+    answer += v + " ";
+
+    for (let nv of [v * 2, v * 2 + 1]) {
+      console.log(nv);
+      if (nv > 7) {
+        continue;
+      }
+
+      queue.push(nv);
+    }
+  }
+
+  return answer;
+}
+
+
+const result = solution();
+console.log(result);
