180f2fba5419bb968873fb2a461fbd0ab2d477f5
Dongwook <dongwook227@gmail.com>
2022-04-07 11:48:34 -0400

    add 9-7: island by bfs

---
+++ inflearn/9-7.js
@@ -0,0 +1,44 @@
+const solution = (board) => {
+  let answer = 0;
+  const n = board.length;
+  const dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  const dy = [0, 1, 1, 1, 0, -1, -1, -1];
+  let queue = [];
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        board[i][j] = 0;
+        queue.push([i, j]);
+        answer++;
+        while (queue.length) {
+          const [x, y] = queue.shift();
+          for (let k = 0; k < 8; k++) {
+            const nx = x + dx[k];
+            const ny = y + dy[k];
+
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+              board[nx][ny] = 0;
+              queue.push([nx, ny]);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return answer;
+}
+
+let board = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0]
+];
+
+const result = solution(board);
+console.log(result);
