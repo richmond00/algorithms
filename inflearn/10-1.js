c8b8de101ab1c059027c901fe95403afec39f0a9
Dongwook <dongwook227@gmail.com>
2022-04-08 11:57:01 -0400

    add 10-1: basic dynamic programming

---
+++ inflearn/10-1.js
@@ -0,0 +1,17 @@
+const solution = (steps) => {
+  let answer = 0;
+  let dy = Array(steps + 1).fill(0);
+  dy[1] = 1;
+  dy[2] = 2;
+
+  for (let i = 3; i <= steps; i++) {
+    dy[i] = dy[i - 1] + dy[i - 2];
+  }
+
+  answer = dy[steps];
+
+  return answer;
+}
+
+const result = solution(7);
+console.log(result);
