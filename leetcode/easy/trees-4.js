9bea62c99436eac06ca80ef230dc5da02a55949d
Dongwook <dongwook227@gmail.com>
2022-04-17 06:54:28 -0400

    easy: binary tree level order traversal

---
+++ leetcode/easy/trees-4.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  if (!root) {
+    return [];
+  }
+  let queue = [root];
+  let result = [];
+  
+  while (queue.length) {
+    let length = queue.length;
+    const values = queue.map(node => node.val);
+    result.push(values);
+    
+    while (length--) {
+      const node = queue.shift();
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
+
+  return result;
+};
