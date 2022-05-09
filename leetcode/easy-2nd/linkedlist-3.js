8e75661b024ebe4839a3d749d77547c85f38e680
Dongwook <dongwook227@gmail.com>
2022-05-09 08:31:43 -0400

    easy: reverse linked list

---
+++ leetcode/easy-2nd/linkedlist-3.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let temp;
+  let prev = null;
+  let curr = head;
+
+  while (curr) {
+    temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+
+  return prev;
+};
