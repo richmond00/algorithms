cd6c28b2675d32a9e894c2c34aaca4ae7dd8b3e4
Dongwook <dongwook227@gmail.com>
2023-11-13 16:19:25 -0500

    updated isPlaindrome

--- udemy/strings/isPalindrome.js
+++ udemy/strings/isPalindrome.js
@@ -3,20 +3,20 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  // 1. removing all non-alphanumeric characters
-  var alphanumeric = s.replaceAll(/[^a-zA-Z0-9]/g, "");
+  const lowercase = s.toLowerCase();
+  const alphanumeric = lowercase.replaceAll(/[^a-zA-Z0-9]/g, "");
+  const strArr = alphanumeric.split("");
 
-  // 2. uppcercase to lowercase
-  var strArr = alphanumeric.split("");
-  var lowercases = strArr.map((char) => char.toLowerCase());
+  let left = 0;
+  let right = strArr.length - 1;
 
-  // 3. check forward and backward; compare two characters by 2/length - 1
-  var indexBy = lowercases.length / 2 - 1;
-  for (var i = 0; i <= indexBy; i++) {
-    var isMatching = lowercases[i] === lowercases[lowercases.length - 1 - i];
+  while (left < right) {
+    const isMatching = strArr[left] === strArr[right];
     if (!isMatching) {
       return false;
     }
+    left++;
+    right--;
   }
 
   return true;
