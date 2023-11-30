8b6b134b256a6b2758e979365e84bded69541b07
Dongwook <dongwook227@gmail.com>
2023-11-30 07:40:45 -0500

    binary tree level order traversal (basic dfs)

--- udemy/recursion/levelOrder.js
+++ udemy/recursion/levelOrder.js
@@ -5,48 +5,23 @@
  *     this.left = (left===undefined ? null : left)
  *     this.right = (right===undefined ? null : right)
  * }
- if (!root) {
-        return [];
-    }
-    
-    let queue = [root];
-    let result = [];
-    
-    while (queue.length) {
-        let length = queue.length;
-        const values = queue.map(node => node.val);
-        result.push(values);
-        
-        while (length--) {
-            const node = queue.shift();
-            if (node.left) {
-                queue.push(node.left);
-            }
-            if (node.right) {
-                queue.push(node.right);
-            }
-        }
-    }
-    
-    return result;
- */
-
 /**
  * @param {TreeNode} root
  * @return {number[][]}
  */
 var levelOrder = function (root) {
   let res = [];
-  console.log("root", root);
   let queue = [root];
 
-  while (queue.length > 0) {
-    console.log("queue", queue);
-    const node = queue.shift();
+  while (queue.length) {
+    console.log("queue", queue, queue.length);
+    const currentNode = queue.shift();
+    res.push(currentNode.val);
 
-    if (node.left) queue.push(node.left);
-    if (node.right) queue.push(node.right);
+    if (currentNode.left) queue.push(currentNode.left);
+    if (currentNode.right) queue.push(currentNode.right);
   }
+  console.log("res", res);
 
-  console.log(res);
+  return res;
 };
