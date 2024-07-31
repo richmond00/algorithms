169eae9e3ff41bb180145d157ac22e77e28b4bbc
Dongwook Kim <dongwook227@gmail.com>
2024-07-31 07:41:20 -0400

    binary seach recursion

--- udemy/recursion/binarySearch.js
+++ udemy/recursion/binarySearch.js
@@ -1,46 +1,21 @@
-const array = [1, 3, 3, 5, 5, 5, 8, 9];
-const targetToFind = 5;
+// const array = [1, 3, 3, 5, 5, 5, 8, 9];
+const arr = [1,2,3,4,5,6,8];
+const targetToFind = 7;
 
-const binarySearch = (nums, left, right, target) => {
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    const foundVal = nums[mid];
-    if (foundVal === target) {
-      return mid;
-    } else if (foundVal < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
+function binarySearch(arr, start, end) {
+  if (start > end) return -1;
+  
+  const mid = Math.floor((start + end) / 2);
+
+  if (arr[mid] === targetToFind) {
+    return mid;
+  } else if (arr[mid] > targetToFind) {
+    return binarySearch(arr, start, mid - 1);
+  } else if (arr[mid] < targetToFind) {
+    return binarySearch(arr, mid + 1, end);
   }
 
   return -1;
-};
+}
 
-const searchRange = function (nums, target) {
-  if (nums.length < 1) return [-1, -1];
-  const firstPos = binarySearch(nums, 0, nums.length - 1, target);
-
-  if (firstPos === -1) return [-1, -1];
-
-  let endPos = firstPos,
-    startPos = firstPos,
-    temp1,
-    temp2;
-
-  while (startPos !== -1) {
-    temp1 = startPos;
-    startPos = binarySearch(nums, 0, startPos - 1, target);
-  }
-  startPos = temp1;
-
-  while (endPos !== -1) {
-    temp2 = endPos;
-    endPos = binarySearch(nums, endPos + 1, nums.length - 1, target);
-  }
-  endPos = temp2;
-
-  return [startPos, endPos];
-};
-
-console.log(searchRange(array, targetToFind));
+console.log(binarySearch(arr, 0, arr.length - 1, 5))
